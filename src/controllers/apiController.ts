1f5022ceba591f652f97e384d01f82af8268fbed
THONWELLING <thonwellingdani13@gmail.com>
2022-03-30 23:39:29 -0300

    criando controllers

---
+++ src/controllers/apiController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express"
+
+
+export const ping = (req: Request, res: Response) => {
+  res.json({ pong: true })
+}
+
+export const random = (req: Request, res: Response) => {
+  let randomNumb: number = Math.floor(Math.random() * 10 )
+
+  res.json({ randomNumb })
+
+}
+
+export const name = (req: Request, res: Response) => {
+  let name: string = req.params.name
+  res.json({ name: `You Sent The Name ${name}`})
+}
